84a4d3900560d29efc99192892bc71ee84221f57
AaronStangz <Aaronstangline.as@gmail.com>
2023-04-10 23:45:29 -0400

    Patch 0.8.1

--- SCP-3008/Assets/Scripts/Tool.cs
+++ SCP-3008/Assets/Scripts/Tool.cs
@@ -4,7 +4,9 @@ using UnityEngine;
 
 public class Tool : MonoBehaviour
 {
+    [Header("Timer")]
     public BreakTimer breakTimer;
+    [Header("Tool Speed")]
     public float Speed = 1;
 
     public void UseTool(BreakingDrops drops)
